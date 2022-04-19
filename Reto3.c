71c8b58b7853ceb9f9f4b49d39f05a82048f514e
Ericka González Romero <98375460+ErickaGR11@users.noreply.github.com>
2022-04-19 13:04:33 -0500

    Add files via upload
    corregido

--- Reto3.c
+++ Reto3.c
@@ -1,62 +1,42 @@
 #include <stdio.h>
 #define MAX 3
-void Solicitar_letras(char arr[MAX], int max);
-void imprimir_arreglo(char arr[MAX]);
-int BuscaBinarioIterativo(char arreglo[MAX], char caract_obj);
-void imprimir_result(int result);
-
+void Solicitar_letras(char arr[MAX]);
+char BuscarBinarioRecursivo(char arr[MAX], int obj, int fin,  int inicio);
 int main()
 {
-    char arreglo_letras[MAX];
-    char result;
+    char arr[MAX];
     char caracter_obj;
-    Solicitar_letras(arreglo_letras,  MAX);
-    imprimir_arreglo( arreglo_letras);
+    char result;
 
-    printf("\nDame el caracter objetivo:");
-    scanf("%s", &caracter_obj);
+    Solicitar_letras(arr);
 
-    //  printf("Caracter dado es: %c\n", caracter_obj);
-    result= BuscaBinarioIterativo( arreglo_letras,  caracter_obj);
-    printf("El caracter mayor al objetivo es: %c", result);
+    printf("Dame el objetivo:");
+    fflush(stdin);
+    scanf("%c", &caracter_obj);
+
+    result = BuscarBinarioRecursivo(arr, caracter_obj, MAX, 0);
+    printf("Letra es: %c", result);
+
 }
-
-void Solicitar_letras(char arr[MAX],   int max)
-{
+void Solicitar_letras(char arr[MAX]){
     printf("Dame las letras:\n");
-    for(int i = 0; i < max; i++)
+    for(int i = 0; i < MAX; i++)
     {
         printf("letra [%d]:", i);
         scanf("%s", &arr[i]);
     }
 }
 
-int BuscaBinarioIterativo(char arreglo[MAX], char caract_obj) {
-    int inicio = 0;
-    int fin = MAX - 1;
-    int mitad;
-    int bandera = 0;
-
-    // Mientras los indices no se crucen y no haya encontrado el valor
-    while (inicio <= fin && bandera == 0) {
-        mitad = inicio + (fin - inicio) / 2;
-
-        if (arreglo[mitad] > caract_obj && arreglo[mitad]< MAX || arreglo[mitad] != arreglo[mitad+1])
-            bandera = 1;
-        else if (arreglo[mitad] > caract_obj) // Seguiria buscando en la mitad inferior
-            fin = mitad - 1;
-        else // Seguiria buscando en la mitad superior
-            inicio = mitad + 1;
+char BuscarBinarioRecursivo(char arr[MAX], int letra_Abuscar, int fin,  int inicio){
+    if(inicio==fin){
+        return arr[0];
     }
-    if (bandera == 1) {
-        return arreglo[mitad];
+    int mitad=(fin+inicio)/2;
+    if(arr[mitad]>letra_Abuscar){
+        if(arr[mitad-1]>letra_Abuscar){
+            return BuscarBinarioRecursivo(arr, letra_Abuscar, mitad-1,  inicio);
+        }
+        return arr[mitad];
     }
-    else
-        return arreglo[0];
-}
-
-void imprimir_arreglo(char arr[MAX]){
-    for(int i=0; i<MAX; i++){
-        printf("%c \t", arr[i]);
-    }
+    return BuscarBinarioRecursivo(arr, letra_Abuscar, fin, mitad+1);
 }
