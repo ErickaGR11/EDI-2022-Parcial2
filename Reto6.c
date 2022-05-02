87ae67c3a4ce4ec5244aba58611a1984f66c2ff9
Ericka González Romero <98375460+ErickaGR11@users.noreply.github.com>
2022-05-01 22:12:25 -0500

    Add files via upload

--- Reto6.c
+++ Reto6.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MAX 10 //capacidad de la cola
+#define MAX 5 //capacidad de la cola
 typedef struct cola{
     int datos [MAX+1];
     int inicio;
@@ -19,54 +19,46 @@ int main()
     int valor;
     int k=2;
     inicializacion(&cola);
-
-   r= estaVacia(cola);
-
-    //printf("valor r: %d\n", r);
-
     r= insertar(&cola, 1);
     r= insertar(&cola, 2);
     r= insertar(&cola, 3);
     r= insertar(&cola, 4);
     r= insertar(&cola, 5);
 
-    for (int i = 1; i <= k; ++i) {
+    for (int i = 0; i <= k; ++i) {
         r= eliminar(&cola, &valor);
         if(i!=k)
            insertar(&cola, valor);
     }
-
+    printf("Salida:%d", valor);
     return 0;
 }
 void inicializacion(Cola *c){
-    c->inicio =0;
+    c->inicio=0;
     c->fin=0;
 }
 
 int estaVacia (Cola cola){
     int r=0;
-    if (cola.inicio==cola.fin){
+    if(cola.inicio==cola.fin)
         r=1;
-    }
     return r;
 }
 
 int estaLlena(Cola cola){
-    if (cola.fin==cola.inicio-1 || (cola.inicio==0 && cola.fin==MAX))
+    if(cola.fin == cola.inicio-1 || (cola.inicio == 0 && cola.fin ==MAX))
         return 1;
     return 0;
-
 }
 int insertar(Cola *c, int valor){
     int r=0;
 
-    if (estaLlena(*c)==0){ //Hay espacio para insertar
-        c->datos [c->fin]= valor; //poner el valor dentro del arreglo en la pos fin
-        //mover fin a la sig casilla libre
-        c->fin++;//incremento el valor de fin
+    if(estaLlena(*c)==0){ //Hay espacio para insertar
+        c->datos[c->fin] = valor;//poner el valor dentro del arreglo en la pos fin
+        c->fin++;//mover fin a la sig casilla libre, incremento el valor de fin
 
-        if(c->fin==MAX+1)
-            c->fin=0;
+        if(c->fin == MAX+1)
+            c->fin = 0;
         r=1;
     }
     return r;
@@ -74,7 +66,7 @@ int insertar(Cola *c, int valor){
 
 int eliminar(Cola *c, int *v){
     int r=0;
-    if (estaVacia(*c)){
+    if (!estaVacia(*c)){
         *v= c->datos[c->inicio];
 
         c->inicio= (c->inicio+1)%(MAX+1);
